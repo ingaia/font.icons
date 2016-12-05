c44393fe54338ba159a0944914dce8b886ad0f13
Bruno Belarmino <bb.belarmino@gmail.com>
2016-12-05 16:06:47 -0200

    adicionando uma versão sass

--- gulpfile.js
+++ gulpfile.js
@@ -9,12 +9,20 @@ var brand = 'ingaia-font-icons';
 var fontName = brand + '-' + runTimestamp;
 var cssClass = 'ing';
 
-gulp.task('iconfont', function(){
 
-  gulp.src('dist/', {read: false})
-	  .pipe(clean());
+gulp.task('clean', function(){
+	return gulp.src('dist/', {read: false}).pipe(clean());
+});
 
-  gulp.src(['svg/*.svg'])
+gulp.task('iconfont-css', function(){
+
+  return gulp.src(['svg/*.svg'])
+  	.pipe(iconfontCss({
+	  fontName: fontName,
+	  targetPath: '../css/'+ brand +'.css',
+	  fontPath: '../fonts/',
+	  cssClass: cssClass
+	}))
 	.pipe(iconfontTemplate({
 		fontName: brand,
 		path: 'templates/template.html',
@@ -22,12 +30,6 @@ gulp.task('iconfont', function(){
 		fontPath: 'dist/fonts/',
 		cssClass: cssClass
 	  }))
-	.pipe(iconfontCss({
-	  fontName: fontName,
-	  targetPath: '../css/'+ brand +'.css',
-	  fontPath: '../fonts/',
-	  cssClass: cssClass
-	}))
 	.pipe(iconfont({
 	  fontName: fontName,
 	  normalize:true,
@@ -35,6 +37,34 @@ gulp.task('iconfont', function(){
 	  timestamp: runTimestamp
 	 }))
 	.pipe(gulp.dest('dist/fonts/'));
+
 });
 
-gulp.task('default', ['iconfont']);
+gulp.task('iconfont-sass', function(){
+
+  return gulp.src(['svg/*.svg'])
+	.pipe(iconfontCss({
+	  fontName: fontName,
+	  path: 'scss',
+	  targetPath: '../scss/'+ brand +'.scss',
+	  fontPath: '#{$ingaia-icons-font-path}',
+	  cssClass: cssClass
+	}))
+	.pipe(iconfontTemplate({
+		fontName: brand,
+		path: 'templates/template.html',
+		targetPath: '../css/template.html',
+		fontPath: 'dist/fonts/',
+		cssClass: cssClass
+	  }))
+	.pipe(iconfont({
+	  fontName: fontName,
+	  normalize:true,
+	  formats: ['ttf', 'eot', 'woff', 'woff2', 'svg'],
+	  timestamp: runTimestamp
+	 }))
+	.pipe(gulp.dest('dist/fonts/'));
+
+});
+
+gulp.task('default', ['clean', 'iconfont-css', 'iconfont-sass']);
