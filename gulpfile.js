b9ac81d394db07bd642fe9757e70693330a6d8ae
Bruno Belarmino <bb.belarmino@gmail.com>
2016-12-05 17:00:09 -0200

    melhorias

--- gulpfile.js
+++ gulpfile.js
@@ -2,16 +2,15 @@ var gulp = require('gulp');
 var iconfont = require('gulp-iconfont');
 var iconfontCss = require('gulp-iconfont-css');
 var iconfontTemplate = require('gulp-iconfont-template');
-var clean = require('gulp-clean');
+var del = require('del');
 var runTimestamp = Math.round(Date.now()/1000);
 
 var brand = 'ingaia-font-icons';
 var fontName = brand + '-' + runTimestamp;
 var cssClass = 'ing';
 
-
 gulp.task('clean', function(){
-	return gulp.src('dist/', {read: false}).pipe(clean());
+	return del.sync('dist/');
 });
 
 gulp.task('iconfont-css', function(){
@@ -23,20 +22,13 @@ gulp.task('iconfont-css', function(){
 	  fontPath: '../fonts/',
 	  cssClass: cssClass
 	}))
-	.pipe(iconfontTemplate({
-		fontName: brand,
-		path: 'templates/template.html',
-		targetPath: '../css/template.html',
-		fontPath: 'dist/fonts/',
-		cssClass: cssClass
-	  }))
 	.pipe(iconfont({
 	  fontName: fontName,
 	  normalize:true,
 	  formats: ['ttf', 'eot', 'woff', 'woff2', 'svg'],
 	  timestamp: runTimestamp
 	 }))
-	.pipe(gulp.dest('dist/fonts/'));
+	.pipe(gulp.dest('dist/fonts'));
 
 });
 
@@ -50,6 +42,19 @@ gulp.task('iconfont-sass', function(){
 	  fontPath: '#{$ingaia-icons-font-path}',
 	  cssClass: cssClass
 	}))
+	.pipe(iconfont({
+	  fontName: fontName,
+	  normalize:true,
+	  formats: ['ttf', 'eot', 'woff', 'woff2', 'svg'],
+	  timestamp: runTimestamp
+	 }))
+	.pipe(gulp.dest('dist/fonts'));
+
+});
+
+gulp.task('template-html', function(){
+
+  return gulp.src(['svg/*.svg'])
 	.pipe(iconfontTemplate({
 		fontName: brand,
 		path: 'templates/template.html',
@@ -63,8 +68,21 @@ gulp.task('iconfont-sass', function(){
 	  formats: ['ttf', 'eot', 'woff', 'woff2', 'svg'],
 	  timestamp: runTimestamp
 	 }))
-	.pipe(gulp.dest('dist/fonts/'));
+	.pipe(gulp.dest('dist/fonts'));
 
 });
 
-gulp.task('default', ['clean', 'iconfont-css', 'iconfont-sass']);
+gulp.task('fonts', function(){
+
+  return gulp.src(['svg/*.svg'])
+	.pipe(iconfont({
+	  fontName: fontName,
+	  normalize:true,
+	  formats: ['ttf', 'eot', 'woff', 'woff2', 'svg'],
+	  timestamp: runTimestamp
+	 }))
+	.pipe(gulp.dest('dist/fonts'));
+
+});
+
+gulp.task('default', ['clean', 'iconfont-css', 'iconfont-sass', 'template-html']);
